a96b741ff07a6296aa44921972429241761f7c1c
Timofey Lyadinsky <tima.led12@gmail.com>
2022-11-20 22:55:36 +0300

    feat: add second page object test

--- PageObject/src/test/java/page/CasioHomePage.java
+++ PageObject/src/test/java/page/CasioHomePage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class CasioHomePage extends AbstractPage{
     public static String CASIO_HOME_PAGE_URL = "https://www.casio.co.uk/";
@@ -17,6 +18,16 @@ public class CasioHomePage extends AbstractPage{
 
     public CasioHomePage(WebDriver driver){
         super(driver);
+    }
+
+    public int searchByNameResult(String itemName){
+        WebElement searchBox;
+        searchBox = new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='search' and @placeholder='Search here']")));
+        searchBox.sendKeys("F-91W-1XY");
+        List<WebElement> searchResult = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//li/a/div[@class='klevu-name-desc-l2']/div[@class='klevu-name-l2' and contains(.,'"+itemName+"')]")));
+        return searchResult.size();
     }
 
     @Override
--- PageObject/src/test/java/test/CasioTest.java
+++ PageObject/src/test/java/test/CasioTest.java
@@ -12,6 +12,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import page.CasioCartPage;
+import page.CasioHomePage;
 import page.CasioItemPage;
 
 import java.time.Duration;
@@ -20,12 +21,13 @@ public class CasioTest {
     private WebDriver driver;
     private WebDriverWait wait;
 
+    private String watchesNameForSearch = "F-91W-1XY";
     @BeforeMethod(alwaysRun = true)
     public void browserSetup(){
 
         ChromeOptions options = new ChromeOptions();
 
-        options.addArguments(/*"--headless",*/ "--no-sandbox", "--disable-dev-shm-usage", "--window-size=1920,1080",
+        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--window-size=1920,1080",
                 "--disable-extensions", "--proxy-server='direct://'", "--proxy-bypass-list=*", "--start-maximized",
                 "--disable-gpu", "--ignore-certificate-errors","user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36");
         driver = new ChromeDriver(options);
@@ -45,6 +47,14 @@ public class CasioTest {
         WebElement cartInfo = new CasioCartPage(driver)
                 .getCartInfo();
         Assert.assertNotNull(cartInfo);
+    }
+
+    @Test
+    public void searchResult(){
+        int searchByNameResultSize = new CasioHomePage(driver)
+                .openPage()
+                .searchByNameResult(watchesNameForSearch);
+        Assert.assertTrue(searchByNameResultSize > 0);
     }
 
     @AfterMethod(alwaysRun = true)
